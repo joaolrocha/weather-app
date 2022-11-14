8ef8f23ae9a0892e6ba685862364300f21a8eee8
joaolrocha <joaolrocha07@gmail.com>
2022-11-14 13:45:29 -0300

    :sparkles: weather app heroku

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require("path");
 const app = express();
 
 // Serve os arquivos estáticos da pasta dist (gerada pelo ng build)
-app.use(express.static(__dirname + "/dist/weatherapp"));
+app.use(express.static(__dirname + "/dist/weather-app"));
 
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/weatherapp/index.html"));
+  res.sendFile(path.join(__dirname + "/dist/weather-app/index.html"));
 });
 
 // Inicia a aplicação pela porta configurada
